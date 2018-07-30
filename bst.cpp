f7fc7c97003296c2b61213ca302c78d3f7c81a82
unknown <nihalani0000@gmail.com>
2018-07-30 22:33:32 +0800

    Added Insert Implementation

--- bst.cpp
+++ bst.cpp
@@ -28,12 +28,38 @@ int bst::find(int item_i){
 
 int bst::insert(int item_i){
 
-    node temp = new node;
+    node *temp = new node;
     temp->left_p = NULL;
     temp->right_p = NULL;
     temp->parent_p = NULL;
     temp->value_i = item_i;
+    int found = 0;
     if(root == NULL){
         root = temp;
+    }
+    else{
+        node *currentNode = new node;
+        currentNode = root;
+        currentNode->parent_p = NULL;
+
+        while(found == 0){
+            currentNode->parent_p = currentNode;
+            if(item_i < currentNode->value_i){
+                currentNode = currentNode->left_p;
+
+                if(currentNode == NULL){
+                    currentNode->parent_p->left_p = temp;
+                    return;
+                }
+            }
+            else{
+                currentNode = currentNode->right_p;
+
+                if(currentNode == NULL){
+                    currentNode->parent_p->right_p = temp;
+                    return;
+                }
+            }
+        }
     }
 }
